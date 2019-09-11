bac24e02af1adf918cb288cba440826dad83eb92
eswaribala <Parameswaribala@gmail.com>
2019-09-11 13:39:31 +0530

    Add files via upload

---
+++ WebAppInitializer.java
@@ -0,0 +1,38 @@
+package com.hsbc.banking.configurations;
+
+import javax.servlet.ServletContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+
+
+@Configuration
+@ComponentScan(basePackages={"com.hsbc.banking.*"})
+
+public class WebAppInitializer implements WebApplicationInitializer{
+
+	@Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        //appContext.register(LoggingHandler.class); 
+        appContext.register(WebMVCConfig.class);
+       
+        //appContext.refresh();
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
+                "SpringDispatcher", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+         
+    }
+
+}
